be968b91f91b502117c00704921bb9db50ea31e6
zjh11111 <126587926+zjh11111@users.noreply.github.com>
2023-02-28 23:18:30 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -44,7 +44,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'ABfuZEkvGNM_TSYgGGP_KABENizF8FR59hlmcB_SwIA',
+  CALLBACK_TEMPLATE_ID: '7iePL0eiAMOiAC4RzsRkg-znnkJDS7daZ-W-2OGdYws',
 
   CALLBACK_USERS: [
     {
